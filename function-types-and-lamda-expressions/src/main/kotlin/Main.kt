b948409a76c9983a4276310d76ee8fb4d8b13074
pcodesdev <njugunapm23@gmail.com>
2023-08-25 11:59:01 +0300

    use functions as data types

---
+++ function-types-and-lamda-expressions/src/main/kotlin/Main.kt
@@ -0,0 +1,57 @@
+//Use function types and lambda expressions in Kotlin
+//fun trick(){
+//    println("No treats!")
+//
+//}
+
+//Redefine the function with a lambda expression
+val trick={
+    println("No treats!")
+}
+
+// Use functions as a data type
+//Specify the treat variable's data type as () -> Unit.
+val treat:() -> Unit={
+    println("Have a treat!")
+}
+
+//use of function as return type
+
+fun trickOrTreat(isTreat:Boolean, extraTreat:((Int)-> String)?): () -> Unit{
+if(isTreat){
+    return trick
+}else{
+    if(extraTreat != null) {
+        println(extraTreat(5))
+    }
+    return treat
+}
+
+}
+
+fun main(){
+////    calling a function
+//    trick()
+//    storing a function in a variable
+//    to refer to a function as a value, you need to use the function reference operator (::).
+//    val trickFunction=::trick
+////    now trick is a variable not a function
+//    val trickFunction=trick
+//    trick()
+//    trickFunction()
+//    treat()
+
+    val coins:(Int)->String={
+        quantity->"$quantity quarters"
+    }
+
+    val cupCakes:(Int)->String={
+      "Have a cup cake!"
+    }
+
+    val trickFunction= trickOrTreat(false,coins)
+    val treatFunction=trickOrTreat(true,null)
+    trickFunction()
+    treatFunction()
+
+}
